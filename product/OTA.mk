28571d983b0204072fbe6ce189b50e3ff6fdb4dd
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-31 21:41:56 -0400

    a3: Remove device name OTA script
     * This script is no longer necessary, since the variant name
       is now set by libinit_msm8916

--- product/OTA.mk
+++
@@ -1,4 +0,0 @@
-# OTA scripts
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/releasetools/variant_hook.sh:install/bin/variant_hook.sh \
-	$(LOCAL_PATH)/releasetools/installend/update_device_name.sh:install/bin/installend/update_device_name.sh
