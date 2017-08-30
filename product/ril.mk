0d3ae04e26e1c573e4f6facfa3b74a1afb46d451
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-30 19:21:07 -0400

    product: ril: Build ril from source
    Change-Id: Iaf5fceb2d3f8cb9c5dc7e1f2efbd8756e92f9aa5

--- product/ril.mk
+++ product/ril.mk
@@ -4,3 +4,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.transmitpower=true \
     persist.radio.no_wait_for_card=1 \
     persist.radio.use_se_table_only=1
+
+PRODUCT_PACKAGES += \
+	libril \
+	librilutils \
+	rild
