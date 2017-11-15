9393df480cacc757fe8d10b4cbd61715e79b3c03
Dyneteve <sound0020@gmail.com>
2017-11-15 12:25:10 +0000

    product: Add media_codecs_performance
    Change-Id: If4966593d1604b97b556cfc112d1619b0a0dcc26

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,3 @@
 # Camera
 PRODUCT_PACKAGES += \
     Snap
-
-# Media (camera) configuration files
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
