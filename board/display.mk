c87e0e198813903fc8ee7dd13006d6070794c146
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2020-02-08 20:05:27 +0700

    try something

--- board/display.mk
+++ board/display.mk
@@ -3,5 +3,5 @@ TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+# PRODUCT_AAPT_PREF_CONFIG := xhdpi
